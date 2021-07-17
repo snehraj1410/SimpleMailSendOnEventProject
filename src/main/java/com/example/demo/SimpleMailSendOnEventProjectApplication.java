928167477a352bad89ed9b0245058711cc7f2810
snehraj1407 <snehalshivchhandjava@gmail.com>
2021-07-17 20:21:45 +0530

    your message'sample project

---
+++ src/main/java/com/example/demo/SimpleMailSendOnEventProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMailSendOnEventProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMailSendOnEventProjectApplication.class, args);
+	}
+
+}
